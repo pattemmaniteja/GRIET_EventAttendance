7c88a75b40f6b231600fd87a0485726aa173b705
pattemmaniteja <manitejapattem@gmail.com>
2025-04-19 19:16:20 +0530

    error in deploy

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // MongoDB connection (use env variable for production)
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
