781562d42297cf6624bb391577d55afed1cd9b22
pattemmaniteja <manitejapattem@gmail.com>
2025-04-19 18:49:07 +0530

    deployed

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // MongoDB connection (use env variable for production)
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
